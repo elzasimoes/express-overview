4556339cdc2a89dd8ff23875d577715306f9e464
Elza Simões <elzaesimoes@gmail.com>
2020-05-21 00:15:37 -0400

    #Express Overview - Nodemon Issue

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# express-overview
+#Express Overview
+
+###Commands :alien:
+
+`npm i -y`
+`npm i --save express`
+
+###devDependencies :alien:
+`npm i -g nodemon`
+`npm i --save-dev nodemon`
+`npm start`
+
+**[Issue Nodemon](Issue: https://github.com/remy/nodemon/issues/1544 "Issue Nodemon")**
+
+`> [nodemon] restarting due to changes...`
+
+###PM2 Production
+**[PM2 Doc](https://pm2.keymetrics.io/ "PM2 Doc")** :alien:
+
+`npm i --save pm2`
+`npm run production`
+`npx pm2 monit`
+`npx pm2 status`
+`npx pm2 restart`
+`npx pm2 show 0`
+`npx pm2 restart 0`
+`npx pm2 kill`
+
+    "package.json": {
+    "production": "pm2 start index.js --name appx-backend" }
+
+
--- index.test.js
+++ index.test.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const app = express()
 
+//Use pode ser aplicado para qualquer tipo de requisição, GET, POST
+//Use atende todas as requisições
+//All atende todas as requisições
 
-// Função Middleware
-
-app.use('/teste', (req, res, next) => {
-    console.log('Antes ')
-    next()
-})
-
-app.get('/teste', (req, res, next) => {
-    console.log('Durante')
+app.use('/teste', (req, res) => {
     res.json({
         data: [
             { id: 7, name: 'Ana', Position: 1 },
@@ -22,11 +17,13 @@ app.get('/teste', (req, res, next) => {
         limit: 3,
         status: 200
     })
-    next()
-})
 
-app.use('/teste', (req, res) => {
-    console.log('Depois ')
+    res.json({
+        "name": "iPade 2323",
+        "preço": "21212"
+    })
+
+    res.send('<h1>Olá</h1>')
 })
 
 // Startar Express
